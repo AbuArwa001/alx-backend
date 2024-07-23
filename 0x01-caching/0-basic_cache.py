b1cbcf42c8e126c591b1d16345af9248f54026c5
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-23 20:48:08 +0300

    Conform with pep8

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -2,14 +2,17 @@
 """ Basic Caching """
 BaseCaching = __import__('base_caching').BaseCaching
 
+
 class BasicCache(BaseCaching):
     """BasicCaching class Containing two methods"""
     def __init__(self):
         super().__init__()
+
     def put(self, key, item):
         """Set cache data"""
         if key and item:
             self.cache_data.update({key: item})
+    
     def get(self, key):
         """Get cache data"""
         return self.cache_data.get(key, None)
