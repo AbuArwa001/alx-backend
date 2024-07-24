d155d332b87ac9dcfacd830bce24812183f8b1fc
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-24 15:34:56 +0300

    Reformat put

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""FIFOCache  Caching """
+"""LRU Caching"""
 BaseCaching = __import__("base_caching").BaseCaching
 
 
@@ -10,54 +10,45 @@ def most_recent(key, age_bits, cache_data):
             age_bits.update({key: 3})
             continue
         update = age_bits.get(ky, None)
-        if update and update >= 2:
+        if update is not None and update >= 2:
             update -= 1
             age_bits.update({ky: update})
     return age_bits
 
 
 class LRUCache(BaseCaching):
-    """LRUCache class Containing two methods"""
+    """LRUCache class that inherits
+    from BaseCaching and implements LRU caching"""
 
     def __init__(self):
-        """LRUCache  class __init__ method"""
+        """Initialize the class"""
         super().__init__()
         self.age_bits = {}
-        self.discard = "0"
+        self.discard = None
 
     def put(self, key, item):
-        """Set cache data with proper caching algorithm."""
-        lst = list(self.cache_data)
-        ln = len(lst) + 1
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
 
-        if key and item:
-            if key not in self.cache_data:
-                if ln > BaseCaching.MAX_ITEMS:
-                    for _, ky in enumerate(self.cache_data):
-                        update = self.age_bits.get(ky, None)
-                        if update and update > 0:
-                            update -= 1
-                            self.age_bits.update({ky: update})
-                        elif update == 0 and key not in self.age_bits:
-                            self.discard = ky
-                            self.age_bits.pop(self.discard)
-                    if key not in self.age_bits:
-                        self.cache_data.pop(self.discard)
-                        print(f"DISCARD: {self.discard}")
-            else:
-                self.age_bits = most_recent(
-                    key, self.age_bits, self.cache_data
-                )
-            if key not in self.age_bits:
-                self.age_bits.update({key: len(self.cache_data)})
-            else:
-                self.age_bits.update({key: 3})
-            self.cache_data.update({key: item})
+        if key in self.cache_data:
+            self.age_bits = most_recent(key, self.age_bits, self.cache_data)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                # Find the least recently used item to discard
+                self.discard = min(self.age_bits, key=self.age_bits.get)
+                print(f"DISCARD: {self.discard}")
+                del self.cache_data[self.discard]
+                del self.age_bits[self.discard]
+
+            self.age_bits[key] = 3  # Set age bit for the new item
+
+        self.cache_data[key] = item
+        self.age_bits = most_recent(key, self.age_bits, self.cache_data)
 
     def get(self, key):
         """Get an item by key"""
         if key is None or key not in self.cache_data:
             return None
-        # self.age_bits = most_recent(key, self.age_bits, self.cache_data)
-        # return self.cache_data[key]
-        return self.cache_data.get(key, None)
+        self.age_bits = most_recent(key, self.age_bits, self.cache_data)
+        return self.cache_data.get(key)
