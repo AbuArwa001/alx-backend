f88c4daf882923372dd797a051e6e243a6ff8da0
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-30 13:19:19 +0300

    1 app.py

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Initiating module
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+
+
+conf = Config()
+app = Flask(__name__)
+app.config['BABEL_DEFAULT_LOCALE'] = conf.LANGUAGES[0]
+app.config['BABEL_DEFAULT_TIMEZONE'] = "UTC"
+
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """
+    Function to display doc STring
+    """
+    return render_template('0-index.html')
