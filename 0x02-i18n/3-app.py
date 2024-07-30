22020166f8d0ffe641514e5f69143158c0533da1
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-31 01:16:39 +0300

    Add translations - return to decorator

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -22,7 +22,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-# @babel.localeselector
+@babel.localeselector
 def get_locale():
     """
     GET locale of a user
@@ -30,7 +30,8 @@ def get_locale():
     # Use the app configuration directly
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-babel.init_app(app, locale_selector=get_locale)
+# babel.init_app(app, locale_selector=get_locale)
+
 
 @app.route('/')
 def index():
