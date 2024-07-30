a5691e46b0101face945e2933ad6f78cc164004b
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-31 01:09:55 +0300

    Add translations

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+Flask app initialization module
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Configure environs.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+# @babel.localeselector
+def get_locale():
+    """
+    GET locale of a user
+    """
+    # Use the app configuration directly
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+babel.init_app(app, locale_selector=get_locale)
+
+@app.route('/')
+def index():
+    """
+    Renders the home page.
+    """
+    return render_template('2-index.html')
