51c55c17ac15db048e08b76d3437359566e1cb47
AbuArwa001 <khalfanathman12@yahoo.com>
2024-07-30 13:09:32 +0300

    Add requirements

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+"""
+Initiating module
+"""
 from flask import Flask, render_template
 from flask_babel import Babel
 
@@ -7,4 +11,7 @@ babel = Babel(app)
 
 @app.route('/')
 def index():
+    """
+    Function to display doc STring
+    """
     return render_template('0-index.html')
